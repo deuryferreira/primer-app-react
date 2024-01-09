b067a7da669dbcfc8dfc43b6328137eb32576a9a
deuryferreira <deury.fr@gmail.com>
2024-01-09 15:22:04 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import {tasks as data} from "./data/tasks.js" //tasklist source
-import {useState, useEffect} from 'react'
+import { tasks as data } from "./data/tasks.js" //tasklist source
+import { useState, useEffect } from 'react'
 
 import TaskList from './components/TaskList.jsx'
 import TaskForm from './components/TaskForm.jsx'
 
 
 //app
-const App = ()=>{
+const App = () => {
   const [tasks, setTasks] = useState([])
 
-  useEffect(()=>{setTasks(data)},[])
+  useEffect(() => { setTasks(data) }, [])
 
 
   //create task
-  const createTask=(task)=>{
-setTasks([...tasks,{
-  id: tasks.length,
-  title:task.title,
-  description:task.description
-}])
+  const createTask = (task) => {
+    setTasks([...tasks, {
+      id: tasks.length,
+      title: task.title,
+      description: task.description
+    }])
   }
 
-  const deleteTask=(taskID)=>{
-    setTasks(tasks.filter(task=>task.id !== taskID));
-    }
+  const deleteTask = (taskID) => {
+    setTasks(tasks.filter(task => task.id !== taskID));
+  }
 
 
-  return(
-  <div>
-    <TaskForm createTask={createTask}/>
-    <TaskList tasks={tasks} deleteTask={deleteTask}/>
-  </div>
+  return (
+    <div>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
+    </div>
   )
 }
-
+//asd
 export default App;
